1910bf93dceabad8e39d845670d5b5d1d1c765be
Erin Kim <erin.e.kim@gmail.com>
2014-11-02 21:23:46 +0000

    Validation for invalid entries and error message

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => { :maxium => 300, :minimum => 3}
+	validates :author, :presence => true, :length => { :maxium => 100, :minimum =>3}
 end
+
