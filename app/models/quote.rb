45d84be0bf0fd758b07ce5d40dd074da6babfb73
Erin Kim <erin.e.kim@gmail.com>
2014-12-31 17:37:21 +0000

    Created slug functionality

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maxium => 300, :minimum => 3}
 	validates :author, :presence => true, :length => { :maxium => 100, :minimum =>3}
+	before_create :store_slug
 
 	def unique_tag
 		abbr = self.author.split(" ").collect do |sub_string|
@@ -9,5 +10,15 @@ class Quote < ActiveRecord::Base
 
 		abbr.join + '#' + self.id.to_s
 	end 
+
+	def generate_slug
+		slug_author = self.author.downcase.gsub(" ", "-").gsub("'", "")
+		slug_saying = self.saying.split(" ").take(4).join("-").downcase.gsub("'", "")
+		slug_author + '-' + slug_saying 
+	end
+
+	def store_slug
+		self.slug = generate_slug 
+	end
 end
 
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,9 +3,24 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
 	test "unique_tag" do  
 		quote = FactoryGirl.create(:quote, :author => 'Oscar Wilde')
-		puts quote.inspect
 		expected = 'OW#' + quote.id.to_s
 		actual = quote.unique_tag
 		assert_equal expected, actual
 	end
+
+	test "generate slug" do
+		q = Quote.new(:author => 'Oscar Wilde', :saying => "blah blah I don't know what to write I'm a poet")
+		slug = q.generate_slug
+		expected = "oscar-wilde-blah-blah-i-dont"
+		assert_equal expected, slug 
+	end
+
+	test "q.save" do
+		q = Quote.new(:author => 'Oscar Wilde', :saying => "blah blah I don't know what to write I'm a poet")
+		q.save
+		expected = "oscar-wilde-blah-blah-i-dont"
+		# q.reload 
+		assert_equal expected, q.slug
+	end
+
 end
