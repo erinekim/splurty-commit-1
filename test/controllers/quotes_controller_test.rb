00f278e1fd3b42e377ac5181c1fb0bd91227cf4c
Erin Kim <erin.e.kim@gmail.com>
2014-12-31 04:43:07 +0000

    Added test for quotes controller

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quotes show page" do
+     quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+     get :show, :id => quote.id
+   end
+
+  test "quote show page, not found" do
+  	get :show, :id => 'BLAH' 
+  	assert_response :not_found
+  end 
 end
